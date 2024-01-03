88fc3dc82f4ab3e868f362652a61281539dafaa7
ahmed <ahelmyeng1@gmail.com>
2024-01-03 11:48:09 -0800

    task

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,10 +9,9 @@
 
 size_t binary_tree_depth(const binary_tree_t *node)
 {
+size_t depth = 0;
 if (node == NULL)
 return (0);
-
-size_t depth = 0;
 while (node->parent != NULL)
 {
 node = node->parent;
