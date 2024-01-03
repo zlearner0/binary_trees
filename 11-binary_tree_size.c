05da5e909d00c508224cb56c1d0dd0fd36392250
ahmed <ahelmyeng1@gmail.com>
2024-01-03 11:45:39 -0800

    task

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - the size of a binary tree
+ *
+ * @node: Pointer to  root
+ * Return: the tree size
+ */
+
+size_t binary_tree_size(const binary_tree_t *node)
+{
+if (node == NULL)
+{
+return (0);
+}
+size_t size = 1;
+size += binary_tree_size(node->left);
+size += binary_tree_size(node->right);
+return (size);
+}
